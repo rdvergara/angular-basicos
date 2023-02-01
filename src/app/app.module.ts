098163da4a773528f3f6b63bd270b3c21c0b25b5
Reinaldo Vergara <reinaldo.vergara@softtek.com>
2023-02-01 18:55:48 -0300

    Termino seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { contadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HeroesModule,
-    contadorModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        HeroesModule,
+        contadorModule,
+        DbzModule
+    ]
 })
 export class AppModule { }
